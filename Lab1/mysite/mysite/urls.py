b2f8e9dc14b56a73ed58a4f13de7e48477ce5946
unknown <ryan.hill@mydit.ie>
2019-09-19 18:00:22 +0100

    Lab-1-Problem-1

--- Lab1/mysite/mysite/urls.py
+++ Lab1/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""lab1 URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
+
 from django.contrib import admin
 from django.urls import include, path
 
