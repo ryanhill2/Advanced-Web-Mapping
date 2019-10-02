7594de42262e7a3539e672511b150dd5a629ef4a
Ryan Hill <ryan.hill1@mydit.ie#>
2019-10-02 11:20:10 +0100

    lab1 added

--- Lab1/mysite/mysite/settings.py
+++ Lab1/mysite/mysite/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.gis.db.backends.postgis',
+        'NAME': 'database1',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+}
+
 }
 
 
